05cf5d4f03fc66550276017c34bc9db4c5b70ec6
Aleksandar <sasaivanovic11@gmail.com>
2021-08-26 15:55:31 +0200

    v3- saving done

--- MapBook/ViewController.swift
+++ MapBook/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 import MapKit
 import CoreLocation
+import CoreData
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var commentText: UITextField!
     
+    var chosenLatitude = Double()
+    var chosenLongitude = Double()
     
     @IBOutlet weak var mapView: MKMapView!
     //pronalazenje lokacije korisnika
     var locationManager = CLLocationManager()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,18 +38,28 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         gestureRecognizer.minimumPressDuration = 3
         mapView.addGestureRecognizer(gestureRecognizer)
         
-        
+        //hide keyboard
+        let gestureRecognizer2 = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer2)
+    }
+    
+    @objc func hideKeyboard(){
+        view.endEditing(true)
     }
     
     @objc func choseLocation(gestureRecognizer : UILongPressGestureRecognizer){
         if gestureRecognizer.state == .began {
+            
             let touchPoint = gestureRecognizer.location(in: self.mapView)
             let touchedCoordinates = self.mapView.convert(touchPoint, toCoordinateFrom: self.mapView)
             
+            chosenLatitude = touchedCoordinates.latitude
+            chosenLongitude = touchedCoordinates.longitude
+            
             let anotation = MKPointAnnotation()
             anotation.coordinate = touchedCoordinates
-            anotation.title = "New anotation point"
-            anotation.subtitle = "MapBook"
+            anotation.title = nameText.text
+            anotation.subtitle = commentText.text
             self.mapView.addAnnotation(anotation)
             
         }
@@ -59,7 +76,33 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         mapView.setRegion(region, animated: true)
         
     }
-
+    
+    
+    @IBAction func saveClicked(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPlace = NSEntityDescription.insertNewObject(forEntityName: "Places", into: context)
+        
+        
+        newPlace.setValue(UUID(), forKey: "id")
+        newPlace.setValue(nameText.text , forKey: "title")
+        newPlace.setValue(commentText.text , forKey: "subtitle")
+        
+        newPlace.setValue(chosenLongitude, forKey: "longitude")
+        newPlace.setValue(chosenLatitude, forKey: "latitude")
+        
+        
+        do {
+            try context.save()
+            print("saved")
+        } catch {
+            print("error")
+        }
+        
+        
+    }
+    
 
 }
 
